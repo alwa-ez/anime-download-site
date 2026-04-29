9edcba32c8d399d622344fcaadc464e90914c272
Alwaez Grey <binsarkiel@gmail.com>
2026-04-29 23:06:03 +0700

    updated

--- login.js
+++ login.js
@@ -1,10 +1,27 @@
 import { supabase } from './supabase.js'
 
-loginBtn.onclick = async () => {
-  const email = email.value
-  const password = password.value
+const btn = document.getElementById('loginBtn')
+const errorMsg = document.getElementById('errorMsg')
 
-  const { error } = await supabase.auth.signInWithPassword({ email, password })
+btn.onclick = async () => {
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value
 
-  if (!error) location.href = '/admin.html'
+  errorMsg.innerText = ''
+
+  if (!email || !password) {
+    errorMsg.innerText = 'Email dan password wajib diisi'
+    return
+  }
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  })
+
+  if (error) {
+    errorMsg.innerText = 'Login gagal: ' + error.message
+  } else {
+    location.href = '/admin.html'
+  }
 }
