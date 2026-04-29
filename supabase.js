bcc9dfdeb88df8212dee293fba2bed4adcad9337
Alwaez Grey <binsarkiel@gmail.com>
2026-04-29 23:11:58 +0700

    updated

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const supabaseUrl = 'https://eyrlxcfasxdciiovtjdv.supabase.co'
-const supabaseKey = 'sb_publishable_qTaKyqdvHYQrpqUOm01w2A_0ZH06p66'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5cmx4Y2Zhc3hkY2lpb3Z0amR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0Njg2MzUsImV4cCI6MjA5MzA0NDYzNX0.L7unxkUMAhZkyg5poNAfRWTbJ_UVpMDZ_oxwrCvihe0'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
